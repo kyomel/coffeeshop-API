5999318610ca6dcdf496d8acb0c375b6e6a0e3e1
kyomel <michaellapandio04@gmail.com>
2025-03-09 22:58:33 +0700

    fixing template html not view at Golang

--- coffee/coffee.go
+++ coffee/coffee.go
@@ -7,28 +7,44 @@ import (
 )
 
 type CoffeeDetails struct {
-	Name  string
-	Price float64
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
 }
 
 type CoffeeList struct {
-	List []CoffeeDetails
+	List []CoffeeDetails `json:"coffeelist"`
 }
 
 var Coffees CoffeeList
 
 func GetCoffees() (*CoffeeList, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
 	if err != nil {
-		fmt.Println("Error reading config file, %w", err)
+		fmt.Printf("Error reading config file: %v\n", err)
 		return nil, err
 	}
 
-	err = viper.Unmarshal(&Coffees)
+	fmt.Printf("Raw config: %+v\n", viper.AllSettings())
+
+	coffeeListRaw := viper.Get("coffeelist")
+	if coffeeListRaw == nil {
+		fmt.Println("Warning: 'coffeelist' key not found in config")
+		return &CoffeeList{}, nil
+	}
+
+	var coffeeList []CoffeeDetails
+	err = viper.UnmarshalKey("coffeelist", &coffeeList)
 	if err != nil {
+		fmt.Printf("Error unmarshaling coffeelist: %v\n", err)
 		return nil, err
 	}
+
+	fmt.Printf("Unmarshaled coffee list: %+v\n", coffeeList)
+
+	Coffees.List = coffeeList
 
 	return &Coffees, nil
 }
--- main.go
+++ main.go
@@ -10,20 +10,30 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
+	r.LoadHTMLGlob("templates/*.html")
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Welcome to the Coffeeshop!",
 		})
 	})
-	r.GET("/home", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
-	})
+	r.GET("/home", coffeeList)
 	r.GET("/coffee", getCoffee)
 
 	fmt.Println("Starting the server...")
 	r.Run(":8081")
+}
+
+func coffeeList(c *gin.Context) {
+	coffee, _ := coffee.GetCoffees()
+
+	c.HTML(
+		http.StatusOK,
+		"index.html",
+		gin.H{
+			"list": coffee.List,
+		},
+	)
 }
 
 func getCoffee(c *gin.Context) {
